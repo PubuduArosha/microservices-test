35d73f267f71a7329e24d22245f65aa8ae5e94b8
Pubudu Arosha <it18108514@my.sliit.lk>
2021-05-15 13:09:04 +0530

    updated

--- src/main/java/com/sliit/mtit/microservice/orderservice/dto/OrderRequest.java
+++ src/main/java/com/sliit/mtit/microservice/orderservice/dto/OrderRequest.java
@@ -1,4 +1,41 @@
 package com.sliit.mtit.microservice.orderservice.dto;
 
 public class OrderRequest {
+
+    private String fullName;
+    private String age;
+    private String oderType;
+    private String orderDetails;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getOderType() {
+        return oderType;
+    }
+
+    public void setOderType(String oderType) {
+        this.oderType = oderType;
+    }
+
+    public String getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(String orderDetails) {
+        this.orderDetails = orderDetails;
+    }
 }
