b3ab2291cad648849e6d9c7a687035aa4c4ba178
Pubudu Arosha <it18108514@my.sliit.lk>
2021-05-17 02:10:27 +0530

    updated

--- order-service/src/main/java/com/sliit/mtit/microservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/sliit/mtit/microservice/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.sliit.mtit.microservice.orderservice.controller;
 
 import com.sliit.mtit.microservice.orderservice.dto.OrderRequest;
 import com.sliit.mtit.microservice.orderservice.dto.OrderResponse;
+import com.sliit.mtit.microservice.orderservice.service.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -10,15 +12,19 @@ import java.util.UUID;
 @RequestMapping("/orders")
 public class OrderController {
 
+    @Autowired
+    private OrderServiceImpl orderService;
+
     @PostMapping (consumes = "application/json", produces = "application/json")
     public @ResponseBody OrderResponse createOrder(@RequestBody OrderRequest request){
 
         System.out.println("Order Details : " + request);
 
-        var orderResponse = new OrderResponse();
-        orderResponse.setOrderId(UUID.randomUUID().toString());
-        orderResponse.setMessage("Successfully created the order");
-        return orderResponse;
+//        var orderResponse = new OrderResponse();
+//        orderResponse.setOrderId(UUID.randomUUID().toString());
+//        orderResponse.setMessage("Successfully created the order");
+
+        return orderService.createOrder(request);
     }
 
 }
--- order-service/src/main/java/com/sliit/mtit/microservice/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/sliit/mtit/microservice/orderservice/service/OrderServiceImpl.java
@@ -3,7 +3,11 @@ package com.sliit.mtit.microservice.orderservice.service;
 import com.sliit.mtit.microservice.orderservice.dto.OrderRequest;
 import com.sliit.mtit.microservice.orderservice.dto.OrderResponse;
 import com.sliit.mtit.microservice.orderservice.dto.UserCreationRequest;
+import com.sliit.mtit.microservice.orderservice.dto.UserCreationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,11 +24,18 @@ public class OrderServiceImpl {
         var userCreationRequest = new UserCreationRequest();
         userCreationRequest.setAge(orderRequest.getAge());
         userCreationRequest.setFullName(orderRequest.getFullName());
-        restTemplate.postForEntity("http://localhost:8080/users",userCreationRequest);
+        ResponseEntity<UserCreationResponse> userCreationResponse =  restTemplate.postForEntity("http://localhost:8080/users",userCreationRequest, UserCreationResponse.class);
 
         var orderResponse = new OrderResponse();
         orderResponse.setOrderId(UUID.randomUUID().toString());
-        orderResponse.setUserId();
+        orderResponse.setUserId(userCreationResponse.getBody().getUserId());
         orderResponse.setMessage("Successfully created the order");
+
+        return orderResponse;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder){
+        return builder.build();
     }
 }
